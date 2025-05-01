788d6de53d06434f39cf8ce24d0964cf17b84089
wiellona <wiellona.darlene@ui.ac.id>
2025-05-02 00:00:33 +0700

    changing link BE

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Axios = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_BASE,
   headers: {
     "Content-Type": "application/json",
   },
