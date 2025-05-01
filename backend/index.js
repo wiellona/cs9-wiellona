6cbc43f35066f735a4c862b58114ee4fa2aa3893
wiellona <wiellona.darlene@ui.ac.id>
2025-05-02 00:23:47 +0700

    adding origin to be

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ const port = process.env.PORT || 3000;
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://os.netlabdte.com"],
+  origin: [
+    "http://localhost:5173",
+    "https://os.netlabdte.com",
+    "https://cs9-wiellona.vercel.app",
+  ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
